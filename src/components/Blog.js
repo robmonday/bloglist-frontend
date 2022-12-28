4a586fd749420ec86cf89da3c827286485e3c139
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-12-28 13:43:19 -0500

    exercise 5.7 completed

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} &nbsp;
+      <button onClick={() => setVisible(true)} style={hideWhenVisible}>view</button>
+      <button onClick={() => setVisible(false)} style={showWhenVisible}>hide</button>
+      <div style={showWhenVisible}>
+        <a href={blog.url}>{blog.url}</a><br />
+        {blog.likes || 0} likes <button>like</button><br />
+        {blog.user.name}
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
