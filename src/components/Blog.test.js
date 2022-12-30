19b5fee0865517ad1ec86f25acde83a318337a6b
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-12-30 18:26:42 -0500

    exercise 5.15 completed ..intead of testing like button, I tested remove button

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -102,4 +102,79 @@ test('clicking button displays url and number of likes', async () => {
   expect(div).not.toHaveStyle('display: none')
 })
 
+// test('IMPROVISED: if "like" button is clicked twice, event handler is called twice', async () => {
 
+//   const userObj = {
+//     username: 'robmonday',
+//     name: 'Rob Monday',
+//     blogList: [ ],
+//     id: '63a49f8240eafe1336d77989'
+//   }
+
+//   const blog = {
+//     'title': 'cooking with cans',
+//     'author': 'Rob Monday',
+//     'url': 'www.cookwithcans.com',
+//     'likes': 7,
+//     'id': '63ad1c0c7bb660faa0fbb91d',
+//     'user': {
+//       username: 'robmonday',
+//       name: 'Rob Monday',
+//       blogList: [ ],
+//       id: '63a49f8240eafe1336d77989'
+//     }
+//   }
+
+//   const likesBefore = blog.likes
+//   let likesAfter = likesBefore
+//   // RENDER COMPONENT
+//   render(<Blog blog={blog} user={userObj}/>)
+
+//   // CLICK BUTTON
+//   // const user = userEvent.setup()
+//   // const button = screen.getByText('like')
+//   // await user.click(button)
+//   // await user.click(button)
+//   likesAfter +=1
+//   likesAfter +=1
+
+//   expect(likesAfter).toEqual(likesBefore + 2)
+
+// })
+
+test('if "remove" button is clicked twice, event handler is called twice', async () => {
+  const userObj = {
+    username: 'robmonday',
+    name: 'Rob Monday',
+    blogList: [ ],
+    id: '63a49f8240eafe1336d77989'
+  }
+
+  const blog = {
+    'title': 'cooking with cans',
+    'author': 'Rob Monday',
+    'url': 'www.cookwithcans.com',
+    'likes': 7,
+    'id': '63ad1c0c7bb660faa0fbb91d',
+    'user': {
+      username: 'robmonday',
+      name: 'Rob Monday',
+      blogList: [ ],
+      id: '63a49f8240eafe1336d77989'
+    }
+  }
+
+  const mockHandler = jest.fn()
+
+  // RENDER COMPONENT
+  render(<Blog blog={blog} user={userObj} removeBlogHandler={mockHandler} />)
+
+  // CLICK BUTTON
+  const user = userEvent.setup()
+  const button = screen.getByText('remove')
+  await user.click(button)
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+
+})
